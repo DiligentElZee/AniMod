f4d356cc0212a43b91d111b389a11706dce4b966
DiligentElZee <diligent_elzee@icloud.com>
2023-07-18 23:51:41 +0200

    Ore generation added

--- src/main/java/net/mccreators/animod/worldgen/ModConfiguredFeatures.java
+++ src/main/java/net/mccreators/animod/worldgen/ModConfiguredFeatures.java
@@ -6,20 +6,43 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
+
+import java.util.List;
 
 public class ModConfiguredFeatures {
     public static final ResourceKey<ConfiguredFeature<?, ?>> WIP_KEY = registerKey("wip");
 
+    public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_PLACEHOLDER_ORE_KEY = registerKey("placeholder_ore");
+    public static final ResourceKey<ConfiguredFeature<?, ?>> END_PLACEHOLDER_ORE_KEY = registerKey("end_placeholder_ore");
+    public static final ResourceKey<ConfiguredFeature<?, ?>> NETHER_PLACEHOLDER_ORE_KEY = registerKey("nether_placeholder_ore");
+
     public static void bootstrap(BootstapContext<ConfiguredFeature<?, ?>> context) {
+
+        RuleTest stoneReplacables = new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES);
+        RuleTest deepslateReplacables = new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
+        RuleTest netherrackReplacables = new BlockMatchTest(Blocks.NETHERRACK);
+        RuleTest endstoneReplacables = new BlockMatchTest(Blocks.END_STONE);
+
+        List<OreConfiguration.TargetBlockState> overworldPlaceholderOres = List.of(OreConfiguration.target(stoneReplacables,
+                ModBlocks.PLACEHOLDER_ORE.get().defaultBlockState()),
+                OreConfiguration.target(deepslateReplacables, ModBlocks.DEEPSLATE_PLACEHOLDER_ORE.get().defaultBlockState()));
 
         register(context, WIP_KEY, Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
                 BlockStateProvider.simple(ModBlocks.WIP_LOG.get()),
@@ -27,6 +50,13 @@ public class ModConfiguredFeatures {
                 BlockStateProvider.simple(ModBlocks.WIP_LEAVES.get()),
                 new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(1), 3),
                 new TwoLayersFeatureSize(1, 0, 2)).build());
+
+        register(context, OVERWORLD_PLACEHOLDER_ORE_KEY, Feature.ORE, new OreConfiguration(overworldPlaceholderOres, 9));
+        register(context, END_PLACEHOLDER_ORE_KEY, Feature.ORE, new OreConfiguration(endstoneReplacables,
+                ModBlocks.ENDSTONE_PLACEHOLDER_ORE.get().defaultBlockState(), 9));
+        register(context, NETHER_PLACEHOLDER_ORE_KEY, Feature.ORE, new OreConfiguration(netherrackReplacables,
+                ModBlocks.NETHERRACK_PLACEHOLDER_ORE.get().defaultBlockState(), 9));
+
     }
 
 
--- src/main/java/net/mccreators/animod/worldgen/ModPlacedFeatures.java
+++ src/main/java/net/mccreators/animod/worldgen/ModPlacedFeatures.java
@@ -10,7 +10,9 @@ import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 
@@ -19,11 +21,25 @@ import java.util.List;
 public class ModPlacedFeatures {
     public static final ResourceKey<PlacedFeature> WIP_PLACED_KEY = createKey("wip_placed");
 
+    public static final ResourceKey<PlacedFeature> PLACEHOLDER_PLACED_KEY = createKey("placeholder_placed");
+    public static final ResourceKey<PlacedFeature> END_PLACEHOLDER_PLACED_KEY = createKey("end_placeholder_placed");
+    public static final ResourceKey<PlacedFeature> NETHER_PLACEHOLDER_PLACED_KEY = createKey("nether_placeholder_placed");
+
     public static void bootstrap(BootstapContext<PlacedFeature> context) {
         HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
 
         register(context, WIP_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.WIP_KEY),
                 VegetationPlacements.treePlacement(PlacementUtils.countExtra(3, 0.2f, 1), ModBlocks.WIP_SAPLING.get()));
+
+        register(context, PLACEHOLDER_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_PLACEHOLDER_ORE_KEY),
+                ModOrePlacement.commonOrePlacement(16, //veins per chunk
+                        HeightRangePlacement.uniform(VerticalAnchor.absolute(-80), VerticalAnchor.absolute(80))));
+        register(context, END_PLACEHOLDER_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.END_PLACEHOLDER_ORE_KEY),
+                ModOrePlacement.commonOrePlacement(9, //veins per chunk
+                        HeightRangePlacement.uniform(VerticalAnchor.absolute(-80), VerticalAnchor.absolute(80))));
+        register(context, NETHER_PLACEHOLDER_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.NETHER_PLACEHOLDER_ORE_KEY),
+                ModOrePlacement.commonOrePlacement(6, //veins per chunk
+                        HeightRangePlacement.uniform(VerticalAnchor.absolute(-80), VerticalAnchor.absolute(80))));
     }
 
 
