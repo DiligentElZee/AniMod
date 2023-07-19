d7182ea6ae2a9777b6f7e69586abdb94413b18b4
DiligentElZee <diligent_elzee@icloud.com>
2023-07-19 22:29:34 +0200

    "WIP" mob + Mob spawn, Updated to 1.19.4 (UPDATED)

--- src/main/java/net/mccreators/animod/event/ModEvents.java
+++ src/main/java/net/mccreators/animod/event/ModEvents.java
@@ -3,7 +3,11 @@ package net.mccreators.animod.event;
 import net.mccreators.animod.AniMod;
 import net.mccreators.animod.entity.ModEntities;
 import net.mccreators.animod.entity.custom.WIPEntity;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -12,5 +16,10 @@ public class ModEvents {
     @SubscribeEvent
     public static void entityAttributeEvent(EntityAttributeCreationEvent event) {
         event.put(ModEntities.WIP_ENTITY.get(), WIPEntity.setAttributes());
+    }
+    @SubscribeEvent
+    public static void entitySpawnRestriction(SpawnPlacementRegisterEvent event) {
+        event.register(ModEntities.WIP_ENTITY.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                Animal::checkAnimalSpawnRules, SpawnPlacementRegisterEvent.Operation.REPLACE);
     }
 }
